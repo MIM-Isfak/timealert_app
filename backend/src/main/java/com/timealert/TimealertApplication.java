65e1dcf95c977704c6772609954f16e2da68657e
MIM-Isfak <isfak216@gmail.com>
2026-06-06 16:35:29 +0530

    Fix: set timezone to Asia/Colombo

--- backend/src/main/java/com/timealert/TimealertApplication.java
+++ backend/src/main/java/com/timealert/TimealertApplication.java
@@ -1,18 +1,16 @@
 package com.timealert;
 
-import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
-
-import java.security.Security;
+import java.util.TimeZone;
 
 @SpringBootApplication
 @EnableScheduling
 public class TimealertApplication {
 
     public static void main(String[] args) {
-        Security.addProvider(new BouncyCastleProvider());
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Colombo"));
         SpringApplication.run(TimealertApplication.class, args);
     }
 }
